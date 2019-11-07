c44f898b76acf2cd8127adaf3c1ddaf8c9a91db5
coffeepots <coffeepotsgh@gmail.com>
2019-11-07 22:48:05 +0000

    Added rawTimes switch, default now crops nanoseconds
    * When `rawTimes` is passed with "-d:rawTimes" at compilation, time data is untouched and the entire fraction component will be stored in nanoseconds. Milliseconds and microseconds will remain unpopulated.
    * The default behaviour without this switch is now to populate milliseconds and microseconds, and crop nanoseconds.

--- odbc/odbcparams.nim
+++ odbc/odbcparams.nim
@@ -165,14 +165,21 @@ proc readFromBuf(dataItem: var SQLData, buffer: ParamBuffer, indicator: int) =
   of dtBinary: dataItem.binVal = bufToSeq(buffer, indicator)
   of dtTime:
     var timestamp = cast[ptr SQL_TIMESTAMP_STRUCT_FRACTFIX](buffer)
-    const ms = 1_000_000
-    let
-      milliseconds = timestamp.Fraction div ms
-      microseconds = (timestamp.Fraction mod ms) div 1_000
-      # Nanoseconds is not cropped and contains the full nanoseconds to the nearest second.
-      # Weeks is not populated.
-    dataItem.timeVal = initTimeInterval(timestamp.Fraction, microseconds, milliseconds, timestamp.Second, timestamp.Minute, timestamp.Hour,
-        timestamp.Day - 1, 0, timestamp.Month, timestamp.Year) # day is 1 indexed in SQL
+    # The `weeks` field is not populated.
+    when defined(rawTimes):
+      # Don't populate milliseconds/microseconds, and nanoseconds is the uncropped full fraction component.
+      dataItem.timeVal = initTimeInterval(timestamp.Fraction, 0, 0, timestamp.Second, timestamp.Minute, timestamp.Hour,
+          timestamp.Day - 1, 0, timestamp.Month, timestamp.Year) # day is 1 indexed in SQL
+    else:
+      # Default behaviour is to populate milliseconds, microseconds and nanoseconds.
+      const ms = 1e6.int
+      let
+        milliseconds = timestamp.Fraction div ms
+        msRemaining = timestamp.Fraction mod ms
+        microseconds = msRemaining div 1_000
+        nanoseconds = msRemaining mod 1_000
+      dataItem.timeVal = initTimeInterval(nanoseconds, microseconds, milliseconds, timestamp.Second, timestamp.Minute, timestamp.Hour,
+          timestamp.Day - 1, 0, timestamp.Month, timestamp.Year) # day is 1 indexed in SQL
   when defined(odbcdebug):
     echo "Read buffer (first 255 bytes): ", repr(cast[ptr array[0..255, byte]](buffer))
 
